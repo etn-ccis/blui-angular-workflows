d42b846aed56a927e6c200e77a3f8d2272b8fd85
daileytj <daileytj89@gmail.com>
2020-10-14 12:47:49 -0400

    Make the screens responsive, update password strength checker styles

--- login-workflow/src/components/password-strength-checker/password-strength-checker.component.ts
+++ login-workflow/src/components/password-strength-checker/password-strength-checker.component.ts
@@ -6,15 +6,24 @@ import * as Colors from '@pxblue/colors';
     template: `
         <mat-list-item style="height: auto;">
             <mat-icon [style.color]="success ? Colors.blue[500] : Colors.gray[300]">{{ icon }}</mat-icon>
-            <span [style.color]="success ? Colors.gray[300] : Colors.black[500]" style="padding-left: 8px;">{{
-                validationMessage
-            }}</span>
+            <span
+                class="validation-message mat-subheading-1"
+                [style.color]="success ? Colors.gray[300] : Colors.black[500]"
+            >
+                {{ validationMessage }}
+            </span>
         </mat-list-item>
     `,
     styles: [
         `
             ::ng-deep .mat-list-base .mat-list-item .mat-list-item-content {
                 padding: 0;
+            }
+
+            .validation-message {
+                padding-left: 8px;
+                font-weight: 400;
+                line-height: 1.2;
             }
         `,
     ],
