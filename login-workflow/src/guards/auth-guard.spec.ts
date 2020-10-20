85e9ee249c98533426da758ec0f6141463aed168
daileytj <daileytj89@gmail.com>
2020-10-20 08:51:48 -0400

    Fix auth gurad tests

--- login-workflow/src/guards/auth-guard.spec.ts
+++ login-workflow/src/guards/auth-guard.spec.ts
@@ -1,16 +1,23 @@
-// import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { PxbAuthGuard } from './auth-guard';
+import { PxbAuthGuard } from './auth-guard';
 
-// describe('AuthGuard', () => {
-//     let service: PxbAuthGuard;
+describe('AuthGuard', () => {
+    let service: PxbAuthGuard;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(PxbAuthGuard);
-//     });
+    beforeEach(async(() => {
+        void TestBed.configureTestingModule({
+            imports: [RouterTestingModule]
+        }).compileComponents();
+    }));
 
-//     it('should be created', () => {
-//         void expect(service).toBeTruthy();
-//     });
-// });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(PxbAuthGuard);
+    });
+
+    it('should be created', () => {
+        void expect(service).toBeTruthy();
+    });
+});
