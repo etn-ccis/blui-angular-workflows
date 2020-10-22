90620f25f5c2a73c9165f91e2e408cd78345cd14
daileytj <daileytj89@gmail.com>
2020-10-22 10:07:54 -0400

    Cleanup example auth component

--- login-workflow/example/src/app/pages/auth/auth.component.ts
+++ login-workflow/example/src/app/pages/auth/auth.component.ts
@@ -10,13 +10,6 @@ import { PXB_LOGIN_VALIDATOR_ERROR_NAME } from '@pxblue/angular-auth-workflow';
               <div pxb-login-header><img src="assets/images/eaton_stacked_logo.png" style="max-width: 100%; max-height: 80px;"/></div>
               <div pxb-login-footer style="text-align: center;"><img src="assets/images/cybersecurity_certified.png" style="max-width: 30%; align-self: center;"/></div>
             </pxb-login>
-            <pxb-contact-support contactEmail="testEmail@email.com" contactPhone="555-555-5555">  
-            </pxb-contact-support>
-            <pxb-forgot-password contactPhone="555-555-5555">
-            </pxb-forgot-password>
-            <pxb-reset-password></pxb-reset-password>
-            <pxb-create-account></pxb-create-account>
-            <pxb-create-account-invite></pxb-create-account-invite>
         </pxb-auth>
     `,
 })
