d884220220d69716d1bd97171d4981c216ca12e2
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-11-10 16:02:46 -0500

    Fix unit tests

--- login-workflow/src/pages/change-password/change-password.component.spec.ts
+++ login-workflow/src/pages/change-password/change-password.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule } from '@angular/forms';
 import { PxbChangePasswordComponent } from './change-password.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PxbAuthModule } from '../../auth.module';
 
 describe('ChangePasswordComponent', () => {
     let component: PxbChangePasswordComponent;
@@ -9,8 +10,7 @@ describe('ChangePasswordComponent', () => {
 
     beforeEach(() => {
         void TestBed.configureTestingModule({
-            imports: [RouterTestingModule, ReactiveFormsModule],
-            declarations: [PxbChangePasswordComponent],
+            imports: [PxbAuthModule, RouterTestingModule, NoopAnimationsModule],
         }).compileComponents();
     });
 
