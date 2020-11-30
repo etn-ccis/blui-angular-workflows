331056474857178a59d57446c6d8a2c97d6f04c7
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-11-30 09:29:04 -0500

    Reject EULA for error scenario

--- login-workflow/example/src/app/services/register-ui.service.ts
+++ login-workflow/example/src/app/services/register-ui.service.ts
@@ -41,6 +41,7 @@ export class RegisterUIService implements IPxbRegisterUIService {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (registrationCode && registrationCode.toUpperCase() === 'EULA_FAIL') {
+                  return reject();
                 }
                 const eula = SAMPLE_EULA;
                 this._pxbAuthConfig.eula = eula; // This prevents future EULA load requests.
