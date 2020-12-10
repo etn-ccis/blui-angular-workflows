235911a9f7f17a96dd8a8869b8ce991fcfb2ed98
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-12-10 13:18:00 -0500

    Change default routes

--- login-workflow/src/auth/auth.routes.ts
+++ login-workflow/src/auth/auth.routes.ts
@@ -1,8 +1,7 @@
-// TODO: These should be customizable.
 export const LOGIN_ROUTE = 'login';
 export const AUTH_ROUTE = 'auth';
 export const RESET_PASSWORD_ROUTE = 'reset-password';
 export const FORGOT_PASSWORD_ROUTE = 'forgot-password';
-export const CREATE_ACCOUNT_ROUTE = 'create-account';
-export const CREATE_ACCOUNT_INVITE_ROUTE = 'create-account-invite';
-export const CONTACT_SUPPORT_ROUTE = 'contact-support';
+export const CREATE_ACCOUNT_ROUTE = 'register/create-account';
+export const CREATE_ACCOUNT_INVITE_ROUTE = 'register/invite';
+export const CONTACT_SUPPORT_ROUTE = 'support';
