c8ea41ca5c0c738e44e1a03014c500d98b156df6
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-12-11 18:29:12 -0500

    Use mobile stepper

--- login-workflow/src/components/public-api.ts
+++ login-workflow/src/components/public-api.ts
@@ -1,4 +1,3 @@
-export * from './dot-stepper/dot-stepper.component';
 export * from './password-strength-checker/password-strength-checker.component';
 export * from './loading-overlay/loading-overlay.component';
 export * from './auth-common.module';
