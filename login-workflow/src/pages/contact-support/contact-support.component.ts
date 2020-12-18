aa375fe69c0013cbb65e3debfcc66cfc6c0b9455
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-12-18 12:27:57 -0500

    Create a POC for custom support content

--- login-workflow/src/pages/contact-support/contact-support.component.ts
+++ login-workflow/src/pages/contact-support/contact-support.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AUTH_ROUTES } from '../../auth/auth.routes';
 import { PxbAuthConfig } from '../../services/config/auth-config';
@@ -12,7 +12,29 @@ import { PxbAuthConfig } from '../../services/config/auth-config';
     },
 })
 export class PxbContactSupportComponent {
+    @Input() title = 'Contact Us';
+    @Input() buttonText = 'Okay';
+    @Input() generalSupportTitle = 'General Questions';
+    @Input() generalSupportDescription: string;
+    @Input() emergencySupportTitle = 'Emergency Support';
+    @Input() emergencySupportDescription: string;
+
     constructor(private readonly _router: Router, public authConfig: PxbAuthConfig) {}
+
+    ngOnInit(): void {
+        this.generalSupportDescription = `
+        For questions, feedback, or support please email us at
+        <a class="pxb-auth-link" href="mailto:${this.authConfig.contactEmail}">${this.authConfig.contactEmail}</a
+        >.
+`;
+
+        this.emergencySupportDescription = `
+            For technical support, please call
+            <a class="pxb-auth-link" href="tel:${this.authConfig.contactPhone}">
+              ${this.authConfig.contactPhone}
+            </a>.
+          `;
+    }
 
     navigateToLogin(): void {
         void this._router.navigate([`${AUTH_ROUTES.AUTH_WORKFLOW}/${AUTH_ROUTES.LOGIN}`]);
